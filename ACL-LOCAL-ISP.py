b3bc830d4cbf369df2cad69e00326d7757de2694
reamea <cheyreamea7@gmail.com>
2017-01-12 15:00:14 +0700

    ACL-LOCAL-ISP
    access-list for local ISP

---
+++ ACL-LOCAL-ISP.py
@@ -0,0 +1,15 @@
+ip access-list extended acl-local-isp-in
+ remark #ONLINE#
+ permit ip any 203.189.128.0 0.0.31.255
+ permit ip any 124.248.160.0 0.0.31.255
+ permit ip any 202.62.32.0 0.0.31.255
+ permit ip any 103.239.52.0 0.0.3.255
+ permit ip any 43.230.192.0 0.0.3.255
+
+ip access-list extended acl-local-isp-out
+ remark #ONLINE#
+ permit ip 203.189.128.0 0.0.31.255 any
+ permit ip 124.248.160.0 0.0.31.255 any
+ permit ip 202.62.32.0 0.0.31.255 any
+ permit ip 103.239.52.0 0.0.3.255 any
+ permit ip 43.230.192.0 0.0.3.255 any
