0d29ba4722b34bbb46b8afb9a1a5c69815d6d96f
reamea <cheyreamea7@gmail.com>
2017-01-13 14:27:54 +0700

    Correct some subnet that wrong calculator

--- ACL-LOCAL-ISP.py
+++ ACL-LOCAL-ISP.py
@@ -7,16 +7,16 @@ ip access-list extended acl-local-isp-in
  permit ip any 43.230.192.0 0.0.3.255
  remark #EZECOM#
  permit ip any 110.74.192.0 0.0.31.255
- permit ip any 113.130.252.0 0.0.3.255
+ permit ip any 113.130.124.0 0.0.3.255
  permit ip any 119.82.248.0 0.0.7.255
- permit ip any 103.9.252.0 0.0.3.255
- permit ip any 163.47.252.0 0.0.3.255
+ permit ip any 103.9.188.0 0.0.3.255
+ permit ip any 163.47.172.0 0.0.3.255
  remark #MekongNet#
  permit ip any 111.90.176.0 0.0.15.255
  permit ip any 116.212.128.0 0.0.31.255
- permit ip any 203.176.240.0 0.0.15.255
+ permit ip any 203.176.128.0 0.0.15.255
  remark #Metfone#
- permit ip any 36.37.28.0 0.0.127.255
+ permit ip any 36.37.128.0 0.0.127.255
  permit ip any 45.64.124.0 0.0.3.255
  permit ip any 103.253.180.0 0.0.3.255
  permit ip any 111.118.128.0 0.0.31.255
@@ -25,15 +25,15 @@ ip access-list extended acl-local-isp-in
  permit ip any 202.150.0.0 0.0.3.255
  remark #wicam#
  permit ip any 43.245.216.0 0.0.3.255
- permit ip any 49.156.224.0 0.0.31.255
- permit ip any 103.17.252.0 0.0.3.255
- permit ip any 119.15.240.0 0.0.15.255
- permit ip any 202.79.248.0 0.0.7.255
+ permit ip any 49.156.32.0 0.0.15.255
+ permit ip any 103.17.212.0 0.0.3.255
+ permit ip any 119.15.80.0 0.0.15.255
+ permit ip any 202.79.24.0 0.0.7.255
  remark #chunwie#
- permit ip any 103.246.252.0 0.0.3.255
- permit ip any 111.64.240.0 0.0.15.255
- permit ip any 124.108.252.0 0.0.3.255
- permit ip any 150.129.252.0 0.0.3.255
+ permit ip any 103.246.228.0 0.0.3.255
+ permit ip any 111.67.96.0 0.0.15.255
+ permit ip any 124.108.4.0 0.0.3.255
+ permit ip any 150.129.20.0 0.0.3.255
 
 ip access-list extended acl-local-isp-out
  remark #ONLINE#
@@ -44,16 +44,16 @@ ip access-list extended acl-local-isp-out
  permit ip 43.230.192.0 0.0.3.255 any
  remark #EZECOM#
  permit ip 110.74.192.0 0.0.31.255 any
- permit ip 113.130.252.0 0.0.3.255 any
+ permit ip 113.130.124.0 0.0.3.255 any
  permit ip 119.82.248.0 0.0.7.255 any
- permit ip 103.9.252.0 0.0.3.255 any
- permit ip 163.47.252.0 0.0.3.255 any
+ permit ip 103.9.188.0 0.0.3.255 any
+ permit ip 163.47.172.0 0.0.3.255 any
  remark #MekongNet#
  permit ip 111.90.176.0 0.0.15.255 any
  permit ip 116.212.128.0 0.0.31.255 any
- permit ip 203.176.240.0 0.0.15.255 any
+ permit ip 203.176.128.0 0.0.15.255 any
  remark #Metfone#
- permit ip 36.37.28.0 0.0.127.255 any
+ permit ip 36.37.128.0 0.0.127.255 any
  permit ip 45.64.124.0 0.0.3.255 any
  permit ip 103.253.180.0 0.0.3.255 any
  permit ip 111.118.128.0 0.0.31.255 any
@@ -62,12 +62,12 @@ ip access-list extended acl-local-isp-out
  permit ip 202.150.0.0 0.0.3.255 any
  remark #wicam#
  permit ip 43.245.216.0 0.0.3.255 any
- permit ip 49.156.224.0 0.0.31.255 any
- permit ip 103.17.252.0 0.0.3.255 any
- permit ip 119.15.240.0 0.0.15.255 any
- permit ip 202.79.248.0 0.0.7.255 any
+ permit ip 49.156.32.0 0.0.31.255 any
+ permit ip 103.17.212.0 0.0.3.255 any
+ permit ip 119.15.80.0 0.0.15.255 any
+ permit ip 202.79.24.0 0.0.7.255 any
  remark #chunwie#
- permit ip 103.246.252.0 0.0.3.255 any
- permit ip 111.64.240.0 0.0.15.255 any
- permit ip 124.108.252.0 0.0.3.255 any
- permit ip 150.129.252.0 0.0.3.255 any
+ permit ip 103.246.228.0 0.0.3.255 any
+ permit ip 111.67.96.0 0.0.15.255 any
+ permit ip 124.108.4.0 0.0.3.255 any
+ permit ip 150.129.20.0 0.0.3.255 any
